d55b3eb966744abf23ed55afe1dee985ac658382
Patrick Hooper <patrick.hooper.m@outlook.com>
2017-12-08 21:00:41 -0500

    sorting attendance

--- src/models/attendance/attendance.py
+++ src/models/attendance/attendance.py
@@ -2,6 +2,8 @@ from src.common.database import Database
 from src.models.games.game import Game
 from src.models.players.player import Player
 
+import src.models.players.constants as PlayerConstants
+
 __author__ = 'hooper-p'
 
 import uuid
@@ -12,7 +14,7 @@ import src.models.attendance.constants as AttendanceCosntants
 class Attendance(object):
     def __init__(self, game, player, attendance='No', _id=None):
         self.game = Game.get_game_by_id(game)
-        self.player = Player.get_player_by_id(player)
+        self.player = Player.get_player_by_id(player['_id'])
         self.attendance = attendance
         self._id = uuid.uuid4().hex if _id is None else _id
 
@@ -36,7 +38,13 @@ class Attendance(object):
 
     @classmethod
     def get_attendance_by_game(cls, game):
-        return [cls(**elem) for elem in Database.find(AttendanceCosntants.COLLECTION, {"game": game})]
+        all_players = [cls(**elem) for elem in Database.find_and_sort(AttendanceCosntants.COLLECTION, {"game": game},
+                                       [("player.number", 1),("player.name", 1)])]
+        for player in all_players[:]:
+            if player.player.name in PlayerConstants.players_to_remove:
+                all_players.remove(player)
+        return all_players
+
 
     @classmethod
     def yes_attendance_by_game(cls, game):
--- src/models/players/constants.py
+++ src/models/players/constants.py
@@ -1,3 +1,4 @@
 __author__ = 'hooper-p'
 
 COLLECTION = 'players'
+players_to_remove = ['Gary Michael McDaniel', 'Michelle Slade']
