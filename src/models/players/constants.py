48ec137a345b88fec289d4f013947c979ac01cb8
Patrick Hooper <patrick.hooper.m@outlook.com>
2018-05-08 19:41:51 -0400

    add players to remove

--- src/models/players/constants.py
+++ src/models/players/constants.py
@@ -1,6 +1,6 @@
 __author__ = 'hooper-p'
 
 COLLECTION = 'players'
-players_to_remove = ['Gary Michael McDaniel']
+players_to_remove = ['Gary Michael McDaniel', 'Grant Burns']
 players_to_add = ['Patrick Hooper', 'Zach Jahn', 'Lorenzo Ladelfa', 'Dave Kerr', 'Dave Daniels', 'Matt Donaldson',
                   'Ryan Bernier', 'Bob Wood']
