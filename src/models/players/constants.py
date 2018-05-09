118dcb332835ee7778d3e48a608b267400a367cc
Patrick Hooper <patrick.hooper.m@outlook.com>
2018-05-09 19:23:44 -0400

    added too many players

--- src/models/players/constants.py
+++ src/models/players/constants.py
@@ -2,5 +2,5 @@ __author__ = 'hooper-p'
 
 COLLECTION = 'players'
 players_to_remove = ['Gary Michael McDaniel', 'Grant Burns']
-players_to_add = ['Patrick Hooper', 'Zach Jahn', 'Lorenzo Ladelfa', 'Dave Kerr', 'Dave Daniels', 'Matt Donaldson',
+players_to_add = ['Patrick Hooper', 'Zach Jahn', 'Lorenzo Ladelfa', 'Dave Kerr', 'Matt Donaldson',
                   'Ryan Bernier', 'Bob Wood']
